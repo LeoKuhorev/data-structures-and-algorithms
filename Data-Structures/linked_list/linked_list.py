5cb8b119efcf7f8e3b5eeb7896f1acc7ffa7a930
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-06-16 15:57:09 -0700

    tests added, readme updated

--- Data-Structures/linked_list/linked_list.py
+++ Data-Structures/linked_list/linked_list.py
@@ -10,6 +10,12 @@ class Node:
         self.val = val
         self.next = None
 
+    def __str__(self):
+        return f'{{{self.val}}}'
+
+    def __repr__(self):
+        return f'Node({self.val})'
+
 
 class LinkedList:
     """Class Singly Linked List"""
@@ -45,11 +51,74 @@ class LinkedList:
             bool: Whether the value was found
         """
         curr = self.head
-        while not curr is None:
+        while curr:
             if curr.val == target:
                 return True
             curr = curr.next
         return False
+
+    def append(self, val: any) -> None:
+        """Appends a node with the given value to the end of the Linked List
+
+        Args:
+            val (any): Value to be passed in
+        """
+        new_node = Node(val)
+        if self.head:
+            curr = self.head
+            while curr.next:
+                curr = curr.next
+            curr.next = new_node
+        else:
+            self.head = new_node
+
+    def insert_before(self, val: any, new_val: any) -> bool:
+        """Inserts a node with the given new value before the first occurence of the given target value. Returns False otherwise
+
+        Args:
+            val (any): Target value to look for
+            new_val (any): New value to be passed in
+
+        Returns:
+            bool: Whether the insertion was successful
+        """
+        curr = prev = self.head
+        while curr:
+            if curr.val == val:
+                new_node = Node(new_val)
+                if curr == self.head:
+                    self.head = new_node
+                    new_node.next = curr
+                else:
+                    prev.next = new_node
+                    new_node.next = curr
+                return True
+            else:
+                prev, curr = curr, curr.next
+        else:
+            return False
+
+    def insert_after(self, val: any, new_val: any) -> bool:
+        """Inserts a node with the given new value after the first occurence of the given target value. Returns False otherwise
+
+        Args:
+            val (any): Target value to look for
+            new_val (any): New value to be passed in
+
+        Returns:
+            bool: Whether the insertion was successful
+        """
+        curr = self.head
+        while curr:
+            if curr.val == val:
+                new_node = Node(new_val)
+                new_node.next = curr.next
+                curr.next = new_node
+                return True
+            else:
+                curr = curr.next
+        else:
+            return False
 
     def __str__(self) -> str:
         """LL class String representation
