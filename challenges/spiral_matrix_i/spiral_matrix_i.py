eac5d87e8f36005eecc088ab3a035241d9eb6ffb
Leo Kuhorev <47767773+LeoKuhorev@users.noreply.github.com>
2021-01-30 18:32:42 -0800

    Update spiral_matrix_i.py

--- challenges/spiral_matrix_i/spiral_matrix_i.py
+++ challenges/spiral_matrix_i/spiral_matrix_i.py
@@ -7,32 +7,28 @@ class Solution:
         size = rows_end * columns_end
 
         while True:
-            if len(output) == size:
-                break
-
             for j in range(columns_start, columns_end):
                 output.append(matrix[rows_start][j])
             rows_start += 1
-
             if len(output) == size:
                 break
 
             for i in range(rows_start, rows_end):
                 output.append(matrix[i][columns_end - 1])
             columns_end -= 1
-
             if len(output) == size:
                 break
 
             for j in range(columns_end-1, columns_start-1, -1):
                 output.append(matrix[rows_end-1][j])
             rows_end -= 1
-
             if len(output) == size:
                 break
 
             for i in range(rows_end-1, rows_start-1, -1):
                 output.append(matrix[i][columns_start])
             columns_start += 1
+            if len(output) == size:
+                break
 
         return output
